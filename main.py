3c0e4f597d53d17a666c89225683672f0fd7afc6
BruceLeeSon <raketa3322@mail.ru>
2025-11-23 19:52:24 +0300

    add gitignore, make template of game. Add Background.

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Star Wars"
+
+class Game(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        self.bg = arcade.load_texture('background.jpg')
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        self.clear((255,255,255))
+        arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.bg)
+
+    def update(self, delta_time: float):
+        pass
+
+
+window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+window.setup()
+
+arcade.run()
