1257a14f444adb3fdae13dc007556d41e3c0ebc8
BruceLeeSon <raketa3322@mail.ru>
2025-12-07 21:57:04 +0300

    Add win system, enemies that you can kill.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import arcade
+import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Star Wars"
 LASER_SPEED = 5
+ENEMIES_SPEED = 1
+ENEMIES_DISTANCE = 50
 
+class TieFighter(arcade.Sprite):
+    def __init__(self):
+        super().__init__('TieFighter.png', 0.2)
+        self.angle = 90
+        self.change_y = ENEMIES_SPEED
+
+    def update(self):
+        self.center_y -= self.change_y
+        if self.top < 0:
+            self.kill()
 
 class Laser(arcade.Sprite):
     def __init__(self):
@@ -36,28 +49,51 @@ class Game(arcade.Window):
         self.bg = arcade.load_texture('background.jpg')
         self.falcon = Falcon()
         self.lasers = arcade.SpriteList()
+        self.enemies = arcade.SpriteList()
+        self.game = True
+        self.victory = arcade.load_texture('victory.jpg')
+
 
     def setup(self):
-        pass
+        for i in range(50):
+            tie_fighter = TieFighter()
+            tie_fighter.center_x = random.randint(0, SCREEN_WIDTH)
+            tie_fighter.center_y = SCREEN_HEIGHT + i * ENEMIES_DISTANCE
+            self.enemies.append(tie_fighter)
 
     def on_draw(self):
         self.clear((255, 255, 255))
         arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.bg)
         self.falcon.draw()
         self.lasers.draw()
+        self.enemies.draw()
+        if len(self.enemies) == 0:
+            self.game = False
+            arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.victory)
 
     def update(self, delta_time: float):
-        self.falcon.update()
-        self.lasers.update()
+        if self.game == True:
+            self.falcon.update()
+            self.lasers.update()
+            self.enemies.update()
+            for laser in self.lasers:
+                hit_list = arcade.check_for_collision_with_list(laser, self.enemies)
+                if hit_list:
+                    laser.kill()
+                    for enemy in hit_list:
+                        enemy.kill()
+
 
     def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
-        self.falcon.center_x = x
+        if self.game == True:
+            self.falcon.center_x = x
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            laser = Laser()
-            arcade.play_sound(laser.laser_sound, 0.5)
-            self.lasers.append(laser)
+        if self.game == True:
+            if button == arcade.MOUSE_BUTTON_LEFT:
+                laser = Laser()
+                arcade.play_sound(laser.laser_sound, 0.5)
+                self.lasers.append(laser)
 
 
 window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
